2cdeaae6be0e15057a955545842c6ba23d42d9bc
Anna Iwanicka <s27389@pjwstk.edu.pl>
2024-03-12 11:29:12 +0100

    Add Average

--- An-APBD/An-APBD/Program.cs
+++ An-APBD/An-APBD/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("MODYFIKACJA 1");
 Console.WriteLine("MODYFIKACJA 2");
 Console.WriteLine("MODYFIKACJA 3");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double) sum / numbers.Length;
+}
